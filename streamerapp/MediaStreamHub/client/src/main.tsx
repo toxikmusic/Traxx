3f4ce1795837bedd123c210bd131f45cc801a0e2
toxikmusic <153168348+toxikmusic@users.noreply.github.com>
2025-04-13 17:29:05 -0700

    Delete streamerapp/MediaStreamHub directory

--- streamerapp/MediaStreamHub/client/src/main.tsx
+++
@@ -1,6 +0,0 @@
-import "./polyfills"; // Import polyfills first
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
-
-createRoot(document.getElementById("root")!).render(<App />);
