a015ff6f4acf5811afe1a0096d5bde404d03bf81
toxikmusic <153168348+toxikmusic@users.noreply.github.com>
2025-06-02 10:11:39 -0700

    Update db.ts

--- server/db.ts
+++ server/db.ts
@@ -3,7 +3,7 @@ import postgres from 'postgres';
 import * as schema from '@shared/schema';
 
 // Create a PostgreSQL connection
-const connectionString = process.env.DATABASE_URL as string;
+const connectionString = 'postgres://toxik:lJ4-gX6=zP7=jG9+eA9_@traxx-db-1wl46-postgresql.traxx-db-1wl46.svc.cluster.local:5432/traxx-db' as string;
 const sql = postgres(connectionString, { max: 10 });
 
 // Create a drizzle instance with the schema
